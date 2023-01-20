b3353334e99ed9552ba49498b117fb8dacd5d711
andres <andrescorredor20@gmail.com>
2023-01-20 16:02:23 -0500

    numbers_list

--- pairs.py
+++ pairs.py
@@ -1,10 +1,10 @@
 import sys
 
-def find_pairs(numbers, target):
+def find_pairs(numbers_list, target):
     numbers_without_complement = set() # A set to collect the numbers that do not have a partner
     pairs = set() # to collect all pairs that match the target sum.
 
-    for num in numbers:
+    for num in numbers_list:
         # Check if the current number's complement (target - num) is in the set
         complement = target - num
         if complement in numbers_without_complement:
@@ -16,10 +16,10 @@ def find_pairs(numbers, target):
             numbers_without_complement.add(num)
     return pairs
 
-if __name__ == '__main__':
-    try:
-        numbers = set(map(int, sys.argv[1].split(',')))
-        target = int(sys.argv[2])
-        print(find_pairs(numbers, target))
-    except IndexError:
-        print("Error: Please provide a list of numbers and a target as arguments. Example: python pairs.py 1,2,3,4 5")
+
+try:
+    numbers = set(map(int, sys.argv[1].split(',')))
+    target = int(sys.argv[2])
+    print(find_pairs(numbers, target))
+except IndexError:
+    print("Error: Please provide a list of numbers and a target as arguments. Example: python pairs.py 1,2,3,4 5")
